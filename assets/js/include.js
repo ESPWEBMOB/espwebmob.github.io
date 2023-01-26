28ffb2fc090a6ba9cb741e0ce8b6c46298d9e3a5
Willian <Willian@inffoc.com.br>
2023-01-26 11:23:42 -0300

    Adição do Bootstrap e dinamização das páginas com smarty-include entre outros - Willian

---
+++ assets/js/include.js
@@ -0,0 +1,15 @@
+
+
+function loadPage(){
+    let e=document.getElementsByTagName("smarty-include");
+    for(var t=0; t<e.length; t++){
+        let a=e[t];
+        n(e[t].attributes.src.value,function(e){
+                a.insertAdjacentHTML("afterend",e),a.remove()
+        })
+    }
+    function n(e,t){
+        fetch(e).then(e=>e.text()).then(e=>t(e))
+    }
+}
+
